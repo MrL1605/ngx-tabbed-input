80f9a08835fdd9f960d58a64b0d5ed7f50125980
Lalit Umbarkar <lalit.umbarkar9@gmail.com>
2018-02-16 13:05:33 +0530

    AutoComplete has down key listener Added clear all example in app.ts Documented some things

--- src/lib/src/app.ts
+++ src/lib/src/app.ts
@@ -1,34 +1,43 @@
 //our root app component
-import {Component, NgModule, VERSION} from '@angular/core'
+import {Component, NgModule, VERSION, ViewChild} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
 import {FormsModule} from '@angular/forms'
 import {TabbedInputDirective} from './directive';
 
 @Component({
-  selector: 'my-app',
-  template: `
-    <div>
-      <h2>Hello {{name}}</h2>
-      <tabbed-input [initVal]="arr" [options]="autoPopulateOptions" ></tabbed-input>
-    </div>
-  `,
+    selector: 'my-app',
+    template: `
+        <div>
+            <h2>Hello {{name}}</h2>
+            <tabbed-input [onlyOptionsAllowed]="true" [initVal]="arr"
+                          [options]="autoPopulateOptions" #tabbedInput>
+            </tabbed-input>
+            
+            <div class="btn btn-default" (click)="cleanCards()" >Clear All</div>
+            
+        </div>
+    `,
 })
 export class App {
-  
-    arr : Array<string> = [];
-    name: string = "";
-    autoPopulateOptions: Array<string> = ["Option1","Oops2"];
-  
+
+    @ViewChild("tabbedInput") tInput;
+    arr: Array<string> = ['One', 'To2'];
+    name: string = '';
+    autoPopulateOptions: Array<string> = ['Option1', 'Oops2'];
+
     constructor() {
         this.name = `Angular! v${VERSION.full}`;
-        this.arr = ["One", "To2"];
     }
 
+    cleanCards() {
+        this.tInput.clearAll();
+    }
 }
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
-  declarations: [ App, TabbedInputDirective ],
-  bootstrap: [ App ]
+    imports: [BrowserModule, FormsModule],
+    declarations: [App, TabbedInputDirective],
+    bootstrap: [App]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/lib/src/directive.ts
+++ src/lib/src/directive.ts
@@ -1,85 +1,115 @@
 import {Component, Input, OnInit} from '@angular/core'
 
 @Component({
-  selector: 'tabbed-input',
-  template: `
+    selector: 'tabbed-input',
+    template: `
 
-    <div>
-    <div style="display:inline" class="form-control">
-	    <span style="margin-right:5px" class="text-center bg-info" *ngFor="let card of cardsArr; let ind=index"> {{card}}
-            <span (click)="removeCard(card, ind)" class="glyphicon glyphicon-remove small"></span>
-        </span>
-        <input list="browsers"  [(ngModel)]="newCard" name="cardHolder" 
-            (ngModelChange)="updateValue()" (keyup)="displaySuggestionBox()" (keydown)="popIfEmpty($event)"
-            style="border: none; overflow: auto; outline: none;margin: 0px;border: 0px;padding: 0px; background:transparent;" 
-            class="form-control-static">
-    </div>
-    <div>
-    
-    <ul class="hideDiv" style="list-style: none">
-      <li *ngFor="let each of autoPopulateOptions" (click)="autoPopulateClick(each)" >{{each}}</li>
-    </ul>
-
-  `
+        <div>
+            <div style="display:inline" class="form-control">
+                <span style="margin-right:5px" class="text-center bg-info"
+                      *ngFor="let card of cardsArr; let ind=index"> {{card}}
+                    <span (click)="removeCard(card, ind)" class="glyphicon glyphicon-remove small"></span>
+                </span>
+                <input list="browsers" [(ngModel)]="newCard" name="cardHolder"
+                       (ngModelChange)="updateValue()" (keydown)="keyDownListener($event)"
+                       style="border: none; overflow: auto; outline: none;margin: 0;border: 0;padding: 0; background:transparent;"
+                       class="form-control-static">
+            </div>
+            <div>
+                <ul class="hideDiv list-group">
+                    <li *ngFor="let each of autoPopulateOptions; let optionInd=index " class="list-group-item"
+                        (click)="autoPopulateClick(each)" [class.active]="optionInd == selectedInd">{{each}}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    `
 })
 export class TabbedInputDirective implements OnInit {
 
-
-    @Input() initVal : Array<string>;
-    @Input() onlyOptionsAllowed : boolean = true;
+    /*** This array of strings whose cards should already be created on <i>ngOnInit()</i> */
+    @Input() initVal: Array<string>;
+    /*** This option facilitates if a card should only be created from given options */
+    @Input() onlyOptionsAllowed: boolean = false;
+    /*** This array of strings whose autoPopulate should be shown when a user is typing */
     @Input() options: Array<string> = [];
 
-    newCard : string = "";
-    cardsArr : Array<string> = [];
+    newCard: string = '';
+    cardsArr: Array<string> = [];
     autoPopulateOptions: Array<string> = [];
-    displayDiv : string ="none";
+    selectedInd: number = -1;
+
     constructor() {
     }
 
-    ngOnInit(){
-        if (this.initVal){
+    ngOnInit() {
+        if (this.initVal) {
             this.cardsArr = this.initVal;
         }
     }
 
-    clearAll(){
-        this.newCard = "";
+    clearAll() {
+        this.newCard = '';
         this.cardsArr = [];
     }
 
     populateList() {
-      this.autoPopulateOptions = [];
-      for (let each of this.options) {
-          if (this.cardsArr.indexOf(each) == -1)
-              this.autoPopulateOptions.push(each);
-      }
+        this.selectedInd = -1;
+        this.autoPopulateOptions = [];
+        for (let each of this.options) {
+            if (this.cardsArr.indexOf(each) == -1)
+                this.autoPopulateOptions.push(each);
+        }
     }
 
-    autoPopulateClick(val:string) {
-        if (val.trim() == "")
+    /**
+     * Function used to create a card. But mostly used externally by autoPopulate list click
+     * */
+    autoPopulateClick(val: string) {
+        if (val.trim() == '')
             return;
-        this.newCard = "";
+        this.newCard = '';
+        this.selectedInd = -1;
         this.autoPopulateOptions = [];
         if (this.cardsArr.indexOf(val) == -1)
             this.cardsArr.push(val.trim());
     }
 
-    popIfEmpty(e:any) {
-      if (this.newCard.trim() == "" && e.keyCode == 8) {
-          this.cardsArr.pop();
-      }
+    keyDownListener(e: any) {
+        // If Backspace is clicked
+        if (this.newCard.trim() == '' && e.keyCode == 8) {
+            this.cardsArr.pop();
+            return;
+        }
+
+        // If Down is clicked
+        if (e.keyCode == 40 && this.autoPopulateOptions.length != 0) {
+            this.selectedInd = (this.selectedInd + 1) % this.autoPopulateOptions.length;
+        }
+
+        // If Enter is clicked
+        if (e.keyCode == 13 && this.selectedInd != -1) {
+            this.autoPopulateClick(this.autoPopulateOptions[this.selectedInd]);
+        }
     }
 
     updateValue() {
         let current = this.newCard;
+
+        // This first cleans and repopulates the autoPopulate list
+        // as there is a change in value of input field
         this.autoPopulateOptions = [];
         for (let each of this.options) {
-            if (each.toLowerCase().indexOf(current.trim().toLowerCase()) != -1){
-              this.autoPopulateOptions.push(each);
+            if (each.toLowerCase().indexOf(current.trim().toLowerCase()) != -1) {
+                // Only insert given value if it already does not exist in card
+                if (this.onlyOptionsAllowed && this.cardsArr.indexOf(each) == -1)
+                    this.autoPopulateOptions.push(each);
+                else if (!this.onlyOptionsAllowed)
+                    this.autoPopulateOptions.push(each);
             }
         }
-
-        if (current[current.length-1] == " " && current.trim() != "") {
+        console.log(this.autoPopulateOptions);
+        if (current[current.length - 1] == ' ' && current.trim() != '') {
             if (!this.onlyOptionsAllowed) {
                 this.populateList();
                 this.autoPopulateClick(current);
@@ -88,27 +118,27 @@ export class TabbedInputDirective implements OnInit {
                 for (let each of this.autoPopulateOptions) {
                     autoPopInd++;
                     if (each.toLowerCase() == current.trim().toLowerCase()) {
-                      this.autoPopulateClick(this.autoPopulateOptions[autoPopInd]);
-                      return;
+                        this.autoPopulateClick(this.autoPopulateOptions[autoPopInd]);
+                        return;
                     }
                 }
-                this.newCard = "";
+                this.newCard = '';
                 this.populateList();
                 return;
             }
-        } else if (current[current.length-1] == " "){
-            this.newCard = "";
+        } else if (current[current.length - 1] == ' ') {
+            this.newCard = '';
             this.populateList();
-        } else if (current.trim() == "") {
+        } else if (current.trim() == '') {
             this.autoPopulateOptions = [];
         }
     }
 
-    removeCard(cardValue:string, cardInd : number){
+    removeCard(cardValue: string, cardInd: number) {
         this.cardsArr.splice(cardInd, 1);
         this.autoPopulateOptions.push(cardValue);
+        this.selectedInd = -1;
     }
-
 
 
 }
